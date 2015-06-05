7457f8d6d14ebb05c18f0bc9dc7d56a59c207ba4
DeckerCHAN <DeckerCHAN@gmail.com>
2015-06-05 21:36:41 +0800

    Foundation commit.

---
+++ Core/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TerminologyLauncher.GUI;
+
+namespace TerminologyLauncher.Core
+{
+    public class Engine
+    {
+        #region Instance
+
+        public static Engine Instance;
+
+        public static Engine GetEngine()
+        {
+            return Instance ?? (Instance = new Engine());
+        }
+
+        #endregion
+        public UiControl UiControl;
+
+        public Engine()
+        {
+            this.UiControl = new UiControl();
+            this.UiControl.MainWindow.Dispatcher.Invoke(new Action(() => { this.UiControl.MainWindow.Show(); }));
+        }
+        public void Run()
+        {
+            this.UiControl.Run();
+        }
+    }
+}
---
+++ GUI/UiControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace TerminologyLauncher.GUI
+{
+    /// <summary>
+    /// Interaction logic for UiControl.xaml
+    /// </summary>
+    public partial class UiControl
+    {
+        public LoginWindow LoginWindow { get; set; }
+        public UiControl()
+        {
+            this.MainWindow = new MainWindow();
+            this.LoginWindow = new LoginWindow();
+        }
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+}
