034768bf772b71c47ce3778e49d50bd4fef10cd0
DeckerCHAN <DeckerCHAN@gmail.com>
2015-06-07 00:40:51 +0800

    Added Logging system.

---
+++ Logging/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace Logging
+{
+
+        public static class Logger
+        {
+            public static ILog GetLogger()
+            {
+                return LogManager.GetLogger(new StackTrace().GetFrame(1).GetMethod().ReflectedType);
+            }
+        }
+    
+}
