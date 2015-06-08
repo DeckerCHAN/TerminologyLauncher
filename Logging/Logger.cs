3b3ec22beb2485dbf19c42d65167018e0665e380
DeckerCHAN <DeckerCHAN@gmail.com>
2015-06-08 09:59:53 +0800

    Changed namespace.

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
 using log4net;
 
-namespace Logging
+namespace TerminologyLauncher.Logging
 {
 
         public static class Logger
