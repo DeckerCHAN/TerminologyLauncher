5a14d0dbfec4d44d1113c2b5f440e0da7e8a072c
DeckerCHAN <DeckerCHAN@gmail.com>
2015-06-21 22:47:23 +0800

    Upper key and throw new exception during null key.

--- Configs/Config.cs
+++ Configs/Config.cs
@@ -16,13 +16,14 @@ namespace TerminologyLauncher.Configs
         public Config(FileInfo jsonConfigFile)
         {
             this.JsonFileInfo = jsonConfigFile;
-
-
-
         }
 
         public String GetConfig(String key)
         {
+            if (String.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Empty key is not allowed!");
+            }
             this.ReadConfigsFromFile();
             return this.ConfigJObject.SelectToken(key).ToString();
         }
@@ -30,6 +31,11 @@ namespace TerminologyLauncher.Configs
 
         public void SetConfig(String key, String value)
         {
+            if (String.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Empty key is not allowed!");
+            }
+
             if (this.ConfigJObject.SelectToken(key) != null)
             {
                 this.ConfigJObject.SelectToken(key).Replace(new JValue(value));
@@ -41,6 +47,11 @@ namespace TerminologyLauncher.Configs
             }
 
 
+        }
+
+        private String GetUpperKey(String key)
+        {
+            return Char.ToLowerInvariant(key[0]) + key.Substring(1);
         }
 
         private void SaveConfig()
