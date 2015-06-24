9dbbe981975bf1400a9b9f37b64d92baa6fddfac
DeckerCHAN <DeckerCHAN@gmail.com>
2015-06-24 14:29:15 +0800

    Move FileQuery to File Repository!

---
+++ FileRepository/FileRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using TerminologyLauncher.Configs;
+
+namespace TerminologyLauncher.FileRepository
+{
+    public class FileRepository
+    {
+        public String QueryUrl { get; set; }
+        public Config Config { get; set; }
+        public FileRepository()
+        {
+            this.Config = new Config(new FileInfo("Configs/FileRepositoryConfig.json"));
+            this.QueryUrl = this.Config.GetConfig("FileRepositoryUpdateUrl");
+        }
+    }
+}
