672f3fa0b45190559e18c4fdf92b230665ba0353
DeckerCHAN <DeckerCHAN@gmail.com>
2015-07-06 11:56:09 +0800

    Changes accroding to entities changed.

--- FileRepository/FileRepository.cs
+++ FileRepository/FileRepository.cs
@@ -4,6 +4,7 @@ using System.IO;
 using TerminologyLauncher.Configs;
 using TerminologyLauncher.Entities.FileRepository;
 using TerminologyLauncher.Entities.InstanceManagement;
+using TerminologyLauncher.Entities.InstanceManagement.Remote;
 using TerminologyLauncher.Entities.SerializeUtils;
 using TerminologyLauncher.Logging;
 using TerminologyLauncher.Utils;
@@ -14,46 +15,50 @@ namespace TerminologyLauncher.FileRepository
     {
         public String RepoUrl { get; set; }
         public Config Config { get; set; }
-        public Dictionary<String, OfficialProvideFile> OfficialProviRdeFilesRepo { get; set; }
+        public Dictionary<String, OfficialFileEntity> OfficialProviRdeFilesRepo { get; set; }
         public FileRepository()
         {
             Logger.GetLogger().Info("Initializing file repo...");
             this.Config = new Config(new FileInfo("Configs/FileRepositoryConfig.json"));
             this.RepoUrl = this.Config.GetConfig("FileRepositoryUrl");
-            this.OfficialProviRdeFilesRepo = new Dictionary<string, OfficialProvideFile>();
-            DownloadUtils.DownloadFile(this.RepoUrl, this.Config.GetConfig("RepoFilePath"));
-            var repo = JsonConverter.Parse<FileRepo>(System.IO.File.ReadAllText(this.Config.GetConfig("RepoFilePath")));
-            foreach (var officialProvideFile in repo.Files)
-            {
-                this.OfficialProviRdeFilesRepo.Add(officialProvideFile.Id, officialProvideFile);
-            }
+            this.OfficialProviRdeFilesRepo = new Dictionary<string, OfficialFileEntity>();
             Logger.GetLogger().Info("Initialized file repo!");
 
         }
 
-        public void GetOfficialFile(String rootPath, OfficialFileBase officialFileBase)
+        public void LoadRepo()
+        {
+            DownloadUtils.DownloadFile(this.RepoUrl, this.Config.GetConfig("RepoFilePath"));
+            var repo = JsonConverter.Parse<FileRepositoryEntity>(File.ReadAllText(this.Config.GetConfig("RepoFilePath")));
+            foreach (var officialProvideFile in repo.Files)
+            {
+                this.OfficialProviRdeFilesRepo.Add(officialProvideFile.ProvideId, officialProvideFile);
+            }
+        }
+
+        public void GetOfficialFile(String rootPath, OfficialFileEntity officialFileBase)
         {
             //Try to find file at file repo 
-            if (!this.OfficialProviRdeFilesRepo.ContainsKey(officialFileBase.Id))
+            if (!this.OfficialProviRdeFilesRepo.ContainsKey(officialFileBase.ProvideId))
             {
                 throw new Exception(String.Format("Can not find official file:{0} with id:{1} at current repo. Try to change repo or contact instance author to correct provide info."));
                 return;
             }
 
-            var officialRemoteFile = this.OfficialProviRdeFilesRepo[officialFileBase.Id];
+            var officialRemoteFile = this.OfficialProviRdeFilesRepo[officialFileBase.ProvideId];
 
-            DownloadUtils.DownloadFile(officialRemoteFile.DownloadPath, officialFileBase.LocalPath, officialRemoteFile.Md5);
-            Logger.GetLogger().Info(String.Format("Successfully downloaded file:{0} from remote url:{1}.", officialRemoteFile.DownloadPath, rootPath + officialFileBase.LocalPath));
+            DownloadUtils.DownloadFile(officialRemoteFile.DownloadLink, officialFileBase.LocalPath, officialRemoteFile.Md5);
+            Logger.GetLogger().Info(String.Format("Successfully downloaded file:{0} from remote url:{1}.", officialRemoteFile.DownloadLink, rootPath + officialFileBase.LocalPath));
 
         }
 
-        public void GetCustomFile(String rootPath, CustomFileBase customFileBase)
+        public void GetCustomFile(String rootPath, CustomFileEntity customFileBase)
         {
-            DownloadUtils.DownloadFile(customFileBase.DownloadPath, customFileBase.LocalPath, customFileBase.Md5);
-            Logger.GetLogger().Info(String.Format("Successfully downloaded file:{0} from remote url:{1}.", customFileBase.DownloadPath, rootPath + customFileBase.LocalPath));
+            DownloadUtils.DownloadFile(customFileBase.DownloadLink, customFileBase.LocalPath, customFileBase.Md5);
+            Logger.GetLogger().Info(String.Format("Successfully downloaded file:{0} from remote url:{1}.", customFileBase.DownloadLink, rootPath + customFileBase.LocalPath));
         }
 
-        public void GetEntirePackage(DirectoryInfo packRootFolder, EntirePackageFile entirePackageFile)
+        public void GetEntirePackage(DirectoryInfo packRootFolder, EntirePackageFileEntity entirePackageFile)
         {
             DownloadUtils.DownloadZippedFile(entirePackageFile.DownloadPath, packRootFolder.FullName, entirePackageFile.Md5);
             Logger.GetLogger().Info(String.Format("Successfully downloaded file:{0} then extracted to {1}.", entirePackageFile.DownloadPath, packRootFolder.FullName));
