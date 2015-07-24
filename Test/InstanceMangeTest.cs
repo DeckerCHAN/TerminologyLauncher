4f55cd94fbbf690b3368f89d3952b399fab3b23a
DeckerCHAN <DeckerCHAN@gmail.com>
2015-07-25 07:14:21 +0800

    Take some tests.

--- Test/InstanceMangeTest.cs
+++ Test/InstanceMangeTest.cs
@@ -1,66 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TerminologyLauncher.FileRepositorySystem;
 using TerminologyLauncher.InstanceManagerSystem;
+using TerminologyLauncher.Utils;
 using TerminologyLauncher.Utils.ProgressService;
 
-namespace TerminologyLauncher.UnitTest
+namespace Test
 {
-    [TestClass]
-    public class InstanceManagerTest
+    public class InstanceMangeTest
     {
         private static InstanceManager InstanceManager { get; set; }
 
-        [TestMethod]
-        public void MainTest()
+        public static void MainTest()
         {
-            Directory.Delete("Instances", true);
-            this.Initialize();
-            this.LoadInstance();
-            this.AddInstance();
-            this.LaunchInstance();
+            if (Directory.Exists("Instances"))
+            {
+                FolderUtils.DeleteDirectory("Instances");
+            }
+            Initialize();
+            LoadInstance();
+            AddInstance();
+            LaunchInstance();
             // this.RemoveInstance();
         }
 
 
-        public void Initialize()
+        public static void Initialize()
         {
             var fileRepo = new FileRepository("Configs/FileRepositoryConfig.json");
             InstanceManager = new InstanceManager("Configs/InstanceManagerConfig.json", fileRepo);
             Assert.IsTrue(new DirectoryInfo("Instances").Exists);
         }
 
-        public void LoadInstance()
+        public static void LoadInstance()
         {
             InstanceManager.LoadInstancesFromInstanceFolder();
         }
 
-        public void AddInstance()
+        public static void AddInstance()
         {
-            InstanceManager.AddNewInstance("http://terminology.b0.upaiyun.com/PureMC.json");
+            InstanceManager.AddNewInstance("http://192.168.1.103/PureMC.json");
             Assert.IsTrue(new DirectoryInfo("Instances").GetDirectories().Length != 0);
             Assert.IsTrue(new DirectoryInfo("Instances").GetDirectories()[0].GetFiles("*.png").Length == 2);
         }
 
-        public void RemoveInstance()
+        public static void RemoveInstance()
         {
             InstanceManager.RemoveInstance(0);
             Assert.IsTrue(new DirectoryInfo("Instances").GetDirectories().Length == 0);
         }
 
-        public void LaunchInstance()
+        public static void LaunchInstance()
         {
             if (InstanceManager.Config != null)
                 InstanceManager.Config.SetConfig("javaPath", "C:\\jdk1.7.0_51\\bin\\java.exe");
-
+            var fileRepo = new FileRepository("Configs/FileRepositoryConfig.json");
 
             var progress = new InternalNodeProgress();
             progress.ProgressChanged += (i) =>
             {
                 Console.WriteLine(progress.Percent);
             };
-            InstanceManager.LaunchAnInstance(progress.CreateNewInternalSubProgress(100D), 0);
+            var process = InstanceManager.LaunchAnInstance(progress.CreateNewInternalSubProgress(100D), 0);
+            process.WaitForExit();
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            InstanceMangeTest.
+            InstanceMangeTest.MainTest();
         }
     }
 }
