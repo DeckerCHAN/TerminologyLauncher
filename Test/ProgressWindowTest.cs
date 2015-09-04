0de4083ecca0f30b0eec0c95135992c3f2aa024d
DeckerCHAN <DeckerCHAN@gmail.com>
2015-09-04 20:08:48 +0800

    Add progress window test.

---
+++ Test/ProgressWindowTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TerminologyLauncher.GUI;
+using TerminologyLauncher.Utils.ProgressService;
+
+namespace Test
+{
+    public class ProgressWindowTest
+    {
+        public static void MainTest()
+        {
+            var progress = new LeafNodeProgress("Common");
+            ProgressWindow progressWindow = new ProgressWindow(progress);
+
+            var t = Task.Run(() =>
+            {
+                do
+                {
+                    Thread.Sleep(1000);
+                    progress.Percent += 5.5D;
+                    Console.WriteLine("Progress update to {0}!", progress.Percent);
+                } while (true);
+            });
+
+            progressWindow.ShowDialog();
+
+            //var t = Task.Run(() =>
+            //{
+            //    progressWindow = new ProgressWindow(progress);
+            //    progressWindow.Show();
+            //    Console.WriteLine("Show progress!");
+            //});
+
+
+
+        }
+
+    }
+}
