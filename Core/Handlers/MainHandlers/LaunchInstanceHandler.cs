638b7e92a175765b6714a8b363fc6f987337f1a6
DeckerCHAN <DeckerCHAN@gmail.com>
2015-09-04 21:59:40 +0800

    Added launch error output.

--- Core/Handlers/MainHandlers/LaunchInstanceHandler.cs
+++ Core/Handlers/MainHandlers/LaunchInstanceHandler.cs
@@ -19,19 +19,40 @@ namespace TerminologyLauncher.Core.Handlers.MainHandlers
 
         public override void HandleEvent(object sender, EventArgs e)
         {
+
             InstanceEntity instance = this.Engine.UiControl.MajorWindow.SelectInstance;
             var progress = new InternalNodeProgress(String.Format("Launching instance {0}", instance.InstanceName));
             var progressWindow = new ProgressWindow(progress);
             var t = Task.Run(() =>
             {
-                this.Engine.GameProcess = this.Engine.InstanceManager.LaunchInstance(progress, instance.InstanceName, this.Engine.AuthServer.CurrentPlayer);
-                this.Engine.GameProcess.Exited += (s, o) =>
+                try
                 {
-                    this.Engine.UiControl.ShowMainWindow();
-                };
-                this.Engine.UiControl.HideMainWindow();
+                    this.Engine.GameProcess = this.Engine.InstanceManager.LaunchInstance(progress, instance.InstanceName,
+                        this.Engine.AuthServer.CurrentPlayer);
+                    this.Engine.GameProcess.Exited += (s, o) =>
+                    {
+                        this.Engine.UiControl.ShowMainWindow();
+                    };
+                    this.Engine.UiControl.HideMainWindow();
+                }
+                catch (Exception ex)
+                {
+                    Logging.Logger.GetLogger().Error(String.Format("Can not launch this instance cause {0}", ex.ToString()));
+                    new PopupWindow(this.Engine.UiControl.MajorWindow, "Can not launch",
+                        "Caused by an internal error, we can not launch this instance right now.").ShowDialog();
+                    try
+                    {
+                        progressWindow.Close();
+                    }
+                    catch (Exception)
+                    {
+                        //ignore
+                    }
+
+                }
             });
             progressWindow.ShowDialog();
+
         }
 
 
