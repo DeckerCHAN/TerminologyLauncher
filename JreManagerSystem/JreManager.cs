4a1fcbda678d2cd62898b9db366a6e8711daf00a
DeckerCHAN <DeckerCHAN@gmail.com>
2015-09-27 23:04:16 +0800

    Added Jre manager.

---
+++ JreManagerSystem/JreManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminologyLauncher.Configs;
+
+namespace TerminologyLauncher.JreManagerSystem
+{
+    public class JreManager
+    {
+        public Config Config { get; set; }
+
+        public JreManager(String configPath)
+        {
+            this.Config =new Config(new FileInfo(configPath));
+        }
+
+    }
+}
