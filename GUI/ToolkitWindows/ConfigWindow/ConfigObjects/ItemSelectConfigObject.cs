ff768bc6934c0d32c132b09a40768078999b1da0
DeckerCHAN <DeckerCHAN@gmail.com>
2015-09-27 15:53:41 +0800

    Added two types of config object.

---
+++ GUI/ToolkitWindows/ConfigWindow/ConfigObjects/ItemSelectConfigObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminologyLauncher.GUI.ToolkitWindows.ConfigWindow.ConfigObjects
+{
+    public class ItemSelectConfigObject : ConfigObject
+    {
+        private int RealSelectIndex;
+        private LinkedList<string> AvailableAvailableSelectionsValue;
+
+        public LinkedList<String> AvailableSelections
+        {
+            get { return this.AvailableAvailableSelectionsValue; }
+            set
+            {
+                this.AvailableAvailableSelectionsValue = value; 
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ItemSelectConfigObject(String name, String key, LinkedList<String> availableSelections, Int32 defaultSelection)
+            : base(name, key)
+        {
+            this.AvailableSelections = availableSelections;
+            this.SelectIndex = defaultSelection;
+        }
+
+        public Int32 SelectIndex
+        {
+            get { return this.RealSelectIndex; }
+            set
+            {
+                this.RealSelectIndex = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ GUI/ToolkitWindows/ConfigWindow/ConfigObjects/RangeRestrictedSelectConfigObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminologyLauncher.GUI.ToolkitWindows.ConfigWindow.ConfigObjects
+{
+    public class RangeRestrictedSelectConfigObject : ConfigObject
+    {
+        private double MaxiumValue;
+        private double MiniumValue;
+        private double RealValue;
+
+        private Double Maxium
+        {
+            get { return this.MaxiumValue; }
+            set
+            {
+                this.MaxiumValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private Double Minium
+        {
+            get { return this.MiniumValue; }
+            set
+            {
+                this.MiniumValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private Double Value
+        {
+            get { return this.RealValue; }
+            set
+            {
+                if (value > this.Maxium || value < this.Minium) throw new InvalidOperationException("Could not set value higher than maxium or less than minium");
+                this.RealValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public RangeRestrictedSelectConfigObject(string name, string key, Double maxium, Double minium, Double defaultValue)
+            : base(name, key)
+        {
+            this.Maxium = maxium;
+            this.Minium = minium;
+            this.Value = defaultValue;
+        }
+    }
+}
