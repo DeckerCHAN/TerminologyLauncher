472c008db6a7e74e71499d70a902cecbe0729850
DeckerCHAN <DeckerCHAN@gmail.com>
2015-10-11 18:29:56 +1100

    Remove useless config and updated transltaions.

--- Core/Handlers/SystemHandlers/ConfigHandler.cs
+++ Core/Handlers/SystemHandlers/ConfigHandler.cs
@@ -20,11 +20,11 @@ namespace TerminologyLauncher.Core.Handlers.SystemHandlers
         {
             try
             {
-                var javaExeConfig = new TextInputConfigObject("Java bin path", "javaBinPath",
+                var javaExeConfig = new TextInputConfigObject(I18n.TranslationProvider.TranslationProviderInstance.TranslationObject.GuiTranslation.ConfigWindowTranslation.JavaPathTranslation, "javaExePath",
                     this.Engine.JreManager.JavaRuntime.JavaPath);
-                var jvmExtraArguments = new TextInputConfigObject("Extra jvm arguments", "extraJvmArguments",
+                var jvmExtraArguments = new TextInputConfigObject(I18n.TranslationProvider.TranslationProviderInstance.TranslationObject.GuiTranslation.ConfigWindowTranslation.ExtraJvmArgumentTranslation, "extraJvmArguments",
                     this.Engine.InstanceManager.Config.GetConfigString("extraJvmArguments"));
-                var memoryconfigs = new RangeRestrictedSelectConfigObject("Max instance memory allocate size(MB)",
+                var memoryconfigs = new RangeRestrictedSelectConfigObject(I18n.TranslationProvider.TranslationProviderInstance.TranslationObject.GuiTranslation.ConfigWindowTranslation.MaxiumMemoryAllocateTranslation,
                     "maxMemorySizeMega", MachineUtils.GetTotalMemoryInMiB(), 512L,
                     Convert.ToInt64(this.Engine.InstanceManager.Config.GetConfigString("maxMemorySizeMega")));
         
