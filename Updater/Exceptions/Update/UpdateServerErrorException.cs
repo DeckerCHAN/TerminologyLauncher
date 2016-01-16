075c5d4c6677851c539ebba2704a852e45b9db4e
DeckerCHAN <DeckerCHAN@gmail.com>
2016-01-17 00:49:32 +0800

    Refactored version check.

---
+++ Updater/Exceptions/Update/UpdateServerErrorException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TerminologyLauncher.Utils.Exceptions;
+
+namespace TerminologyLauncher.Updater.Exceptions.Update
+{
+    public class UpdateServerErrorException:SolutionProvidedException
+    {
+        public UpdateServerErrorException(String message) : base(message, "Try to reinstall or mail to DeckerCHAN@gmail.com to report this problem.")
+        {
+        }
+    }
+}
--- Updater/UpdateManager.cs
+++ Updater/UpdateManager.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using TerminologyLauncher.Configs;
 using TerminologyLauncher.Entities.UpdateManagement;
 using TerminologyLauncher.I18n;
+using TerminologyLauncher.Updater.Exceptions.Update;
 using TerminologyLauncher.Utils;
 using TerminologyLauncher.Utils.ProgressService;
 using TerminologyLauncher.Utils.SerializeUtils;
@@ -13,40 +14,84 @@ namespace TerminologyLauncher.Updater
 {
     public class UpdateManager
     {
-        public String Version { get; private set; }
+        public VersionEntity Version { get; set; }
         public Config Config { get; set; }
-        public UpdateManager(String configPath, String versionNumber)
+        public UpdateManager(String configPath, String coreVersion, Int32 buildNumber)
         {
             this.Config = new Config(new FileInfo(configPath));
-            this.Version = versionNumber;
+            this.Version = new VersionEntity() { BuildNumber = buildNumber, CoreVersion = coreVersion };
         }
 
-        public Boolean CheckUpdateAvailable()
+        public Boolean IsNewVersionAvailable()
         {
-            Logging.Logger.GetLogger().Info("Start to check lanucher version.");
+            try
+            {
+                var latest = this.GetLatestVersion();
+                if (latest.CoreVersion != this.Version.CoreVersion)
+                {
+                    return true;
+                }
+                return latest.BuildNumber != this.Version.BuildNumber;
+            }
+            catch (Exception ex)
+            {
+                Logging.Logger.GetLogger().ErrorFormat("Can not judge update available. Cause:{0}", ex);
+                return false;
+            }
+
+        }
+
+        public String GetUpdateInformationHumanReadable()
+        {
+            try
+            {
+                var latest = this.GetLatestVersion();
+                if (latest.CoreVersion != this.Version.CoreVersion)
+                {
+                    return String.Format("New generation launcher {0}-{1} available.", latest.CoreVersion,
+                        latest.BuildNumber);
+                }
+                else
+                {
+                    if (latest.BuildNumber > this.Version.BuildNumber)
+                    {
+                        return String.Format("Launcher has newer build version {0}", latest.BuildNumber);
+                    }
+                    else if (latest.BuildNumber == this.Version.BuildNumber)
+                    {
+                        return String.Format("You are using the latest version {0} build {1}", latest.CoreVersion, latest.BuildNumber);
+                    }
+                    else
+                    {
+                        return String.Format("The version you are using ({0}) is higher than official build({1}). You may using pre-release version or test version. PLEASE DO NOT DISTRIBUTE THIS VERSION UNLESS AUTHORISED.",
+                            this.Version.BuildNumber, latest.BuildNumber);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.Logger.GetLogger().ErrorFormat("Can not detecte update available. Cause:{0}", ex);
+                return String.Format("Cannot detecte update right now. Cause:{0}", ex.Message);
+            }
+
+        }
+
+        private VersionEntity GetLatestVersion()
+        {
+            Logging.Logger.GetLogger().Info("Start to check lanucher update.");
             var update = JsonConverter.Parse<UpdateEntity>(DownloadUtils.GetWebContent(this.Config.GetConfigString("updateCheckingUrl")));
-            if (update.LatestVersion == null)
+            if (update.LatestVersion == null || String.IsNullOrEmpty(update.LatestVersion.CoreVersion) || update.LatestVersion.BuildNumber.Equals(0) || String.IsNullOrEmpty(update.LatestVersion.DownloadLink) || String.IsNullOrEmpty(update.LatestVersion.Md5))
             {
-                throw new PlatformNotSupportedException("Can not fetch the latest version! May caused by wrong update url or un-supported version!");
+                throw new UpdateServerErrorException("Cannot fetch the latest version!");
             }
-            var result = !String.IsNullOrEmpty(update.LatestVersion.VersionNumber) &&
-                   update.LatestVersion.VersionNumber != this.Version;
-            if (result)
-            {
-                Logging.Logger.GetLogger().InfoFormat("New version {0} available!", update.LatestVersion.VersionNumber);
-            }
-            else
-            {
-                Logging.Logger.GetLogger().Info("No available version.");
-            }
-            return result;
+            return update.LatestVersion;
         }
 
         public String FetchLatestVersionAndStartUpdate(InternalNodeProgress progress)
         {
             progress.Percent = 0D;
 
-            if (!this.CheckUpdateAvailable())
+            if (!this.IsNewVersionAvailable())
             {
                 return TranslationProvider.TranslationProviderInstance.TranslationObject.HandlerTranslation.LanucherUpdateTranslation.NoAvailableUpdate;
             }
@@ -56,7 +101,7 @@ namespace TerminologyLauncher.Updater
 
 
             var updateTempFolder = Path.Combine(FolderUtils.SystemTempFolder.FullName,
-                String.Format("TerminologyLauncher-{0}", update.LatestVersion.VersionNumber));
+                String.Format("TerminologyLauncher-{0}-{1}", update.LatestVersion.CoreVersion, update.LatestVersion.BuildNumber));
             var updateBinaryFolder = Path.Combine(updateTempFolder, "Binary");
             var updaterExecutorFile = Path.Combine(
                 Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Updater.exe");
@@ -74,7 +119,7 @@ namespace TerminologyLauncher.Updater
                 update.LatestVersion.Md5);
             if (!File.Exists(updaterExecutorFile))
             {
-                throw new FileNotFoundException("Can not find updater.exe. You may have to re-install to resolve!");
+                throw new FileNotFoundException("Cannot find updater.exe. You may have to re-install to resolve!");
             }
             File.Copy("Updater.exe", updaterRealExecutorFile, true);
             var updateProcessInfo = new ProcessStartInfo(updaterRealExecutorFile)
@@ -89,8 +134,8 @@ namespace TerminologyLauncher.Updater
             updateProcess.Start();
 
             progress.Percent = 100D;
-            return String.Format(TranslationProvider.TranslationProviderInstance.TranslationObject.HandlerTranslation.LanucherUpdateTranslation.FetchedNewUpdateToVersion, this.Version,
-    update.LatestVersion.VersionNumber);
+            return String.Format(TranslationProvider.TranslationProviderInstance.TranslationObject.HandlerTranslation.LanucherUpdateTranslation.FetchedNewUpdateToVersion, String.Format("{0}-{1}", this.Version.CoreVersion, this.Version.BuildNumber),
+   String.Format("{0}-{1}", this.Version.CoreVersion, this.Version.BuildNumber));
         }
 
     }
