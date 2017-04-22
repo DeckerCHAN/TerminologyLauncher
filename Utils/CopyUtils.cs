0c0fe89d47b9aaf9dbd095fc44012ca93456fda3
DeckerCHAN <DeckerCHAN@gmail.com>
2017-04-23 01:51:48 +1000

    Added deep copy Util.

---
+++ Utils/CopyUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+
+namespace TerminologyLauncher.Utils
+{
+    public static class CopyUtils
+    {
+        public static object MakeCopy(object original)
+        {
+            return MakeCopy(original, original.GetType());
+        }
+
+        public static object MakeCopy(object original, Type targetType)
+        {
+            var copy = Activator.CreateInstance(targetType);
+            var piList =
+                original.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var pi in piList)
+            {
+                var value = pi.GetValue(original);
+                if (value == null)
+                {
+                    continue;
+                }
+                var type = value.GetType();
+                if (type.IsPrimitive || type == typeof(string))
+                {
+                    pi.SetValue(copy, value);
+                }
+                else if ((type.GetInterface("IEnumerable") != null))
+                {
+                    pi.SetValue(copy, Activator.CreateInstance(type, value));
+                }
+                else
+                {
+                    var newValue = MakeCopy(value);
+                    pi.SetValue(copy, newValue);
+                }
+            }
+            return copy;
+        }
+    }
+}
