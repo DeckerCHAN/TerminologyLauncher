11b1262ec172da8ecf44b86b39d18c750f09f88e
DeckerCHAN <DeckerCHAN@gmail.com>
2017-05-05 01:02:35 +1000

    Fixed missing file.

---
+++ AppoverHelper/Structure/Artifact.cs
@@ -0,0 +1,10 @@
+﻿namespace AppoverHelper.Structure
+{
+    public class Artifact
+    {
+        public string fileName { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public int size { get; set; }
+    }
+}
